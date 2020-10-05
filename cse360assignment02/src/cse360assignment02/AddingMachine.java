3c79f5b7f7a60588cecca9e1f51505bb362e7ad2
awesomesoccer <shreyas.narayanan@gmail.com>
2020-10-04 19:13:34 -0700

    Commit of AddingMachine.java with comments

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -1,26 +1,38 @@
 package cse360assignment02;
 
+/* 
+ * */
+
 public class AddingMachine {
   private int total;
   
+  // Constructor
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
  
+  // Return total price
   public int getTotal () {
     return 0;
   }
   
+  // Adds value to total
   public void add (int value) {
+	  // Total += value
   }
 
+  // Subtracts value from total
   public void subtract (int value) {
-  }
+	  // Total -= value
 
+  }
+  
+  // Returns history of transactions, starting with "0"
   public String toString () {
     return "";
   }
 
+  // Clears memory
   public void clear() {
   }
 }
